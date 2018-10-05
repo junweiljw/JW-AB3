ccde86ceb4716ff000e56114d9d0e6855a1a5445
junweiljw <lowjunwei.ljw@gmail.com>
2018-10-05 13:35:05 +0800

    Added non-working Remark command - Missing the remark class - Only can accept the parameters and throws exceptions for the various unacceptable input types

---
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -0,0 +1,56 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+//import seedu.address.model.person.Person;
+
+import static java.util.Objects.requireNonNull;
+//import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.logic.parser.CliSyntax.*;
+//import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+
+public class RemarkCommand extends Command {
+
+    public static final String COMMAND_WORD = "remark";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark for a person specified in the INDEX. "
+            + "Parameters: INDEX (must be a positive integer)"
+            + PREFIX_REMARK + "REMARK"
+            + "Example: " + COMMAND_WORD + " 1"
+            + PREFIX_REMARK + "Likes to sleep";
+
+    public static final String REMARK_PARAMETERS = "Index: %1$d, Remark: %2$s";
+    //public static final String MESSAGE_REMARK_SUCCESS = "New remark added for: %1$s";
+    //public static final String MESSAGE_DELETE_REMARK_SUCCESS = "Remark deleted for: %1$s";
+
+    private final Index targetIndex;
+    private final String remark;
+
+    public RemarkCommand(Index targetIndex, String remark) {
+        requireNonNull(targetIndex, remark);
+
+            this.targetIndex = targetIndex;
+            this.remark = remark;
+    }
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        throw new CommandException(String.format(REMARK_PARAMETERS, targetIndex.getOneBased(), remark));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof RemarkCommand)) {
+            return false;
+        }
+
+        RemarkCommand e = (RemarkCommand) other;
+        return targetIndex.equals(e.targetIndex) && remark.equals(e.remark);
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -0,0 +1,35 @@
+package seedu.address.logic.parser;
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new {@code RemarkCommand} object
+ */
+public class RemarkCommandParser implements Parser {
+    /**
+     * Parses the user input {@code String} of arguments in the context of {@code RemarkCommand}
+     * and returns an object {@code RemarkCommand} for execution
+     * @throws ParseException if user input is not in the expected format
+     */
+    public RemarkCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_REMARK);
+
+        Index targetIndex;
+        try {
+            targetIndex = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (IllegalValueException a) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.REMARK_PARAMETERS), a);
+        }
+
+        String remark = argMultimap.getValue(PREFIX_REMARK).orElse("");
+
+        return new RemarkCommand(targetIndex, remark);
+    }
+}
+
