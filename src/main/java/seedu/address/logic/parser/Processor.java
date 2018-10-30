606e8868bb544520dd839865746a742c073d1076
junweiljw <lowjunwei.ljw@gmail.com>
2018-10-31 00:14:13 +0800

    Processor Version 2.0
    - Changed the methods for sorting, takes into account whitespaces in arguments

--- src/main/java/seedu/address/logic/parser/Processor.java
+++ src/main/java/seedu/address/logic/parser/Processor.java
@@ -43,10 +43,10 @@ public class Processor {
             return null;
         }
         StringBuilder out = new StringBuilder();
-        out.append(s[0]);
+        out.append(s[0].trim());
         for (int i = 1; i < k; i++) {
             if (s[i] != null) {
-                out.append(glue).append(s[i]);
+                out.append(glue).append(s[i].trim());
             }
         }
         return out.toString();
@@ -81,40 +81,54 @@ public class Processor {
         String targetIndex = null;
         String sortedInput;
         String[] sortedArray = new String[10];
+        int tagIndex;
+        String argument;
 
         // identifies the first instance of an integer and saves it as the targetIndex (assuming that the first integer is the index)
         for (int i = 0; i < args.length; i++) {
             if (isInteger(args[i])) {
                 targetIndex = args[i];
             }
+        }
 
+        for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
 
             // "COMMAND_WORD + PREFIX + DATA" argument pattern
             case AddCommand.COMMAND_WORD:
                 // replaces the COMMAND_WORD in the userInput string with an empty string
-                userInput.replace(args[i], "");
-                // splits the remaining userInput by "/" and savs the tokens in a prefixArguments[]
+                userInput = userInput.replace(args[i], "");
+                // splits the remaining userInput by "/" and saves the tokens in a prefixArguments[]
                 String[] prefixArguments = userInput.split("/", 0);
-                int tagIndex = 5;
+                tagIndex = 5;
                 // first index of the array will be command, to be recognised by addressbookParser
                 sortedArray[0] = args[i];
-                for (int j = 0; j < prefixArguments.length; j++) {
+                for (int j = 0; j < (prefixArguments.length - 1); j++) {
                     // assigns the last character of each token as the prefix, since it was tokenized by "/"
                     String prefix = prefixArguments[j].substring(prefixArguments[j].length()-1);
+                    if (j == (prefixArguments.length - 2)) {
+                        argument = prefixArguments[j+1];
+                    } else {
+                        argument = prefixArguments[j+1].substring(0, prefixArguments[j+1].length() - 2);
+                    }
                     switch (prefix) {
                     case ("n"):
                         // j+1 because the prefix belongs to the next token, meaning the arguments are in the next index
                         // removes the last 2 characters of each string in the prefixArguments array, which is a whitespace + prefix
-                        sortedArray[1] = prefixArguments[j+1].substring(0, prefixArguments[j+1].length() - 2);
+                        sortedArray[1] = "n/" + argument;
+                        break;
                     case ("p"):
-                        sortedArray[2] = prefixArguments[j+1].substring(0, prefixArguments[j+1].length() - 2);
+                        sortedArray[2] = "p/" + argument;
+                        break;
                     case ("e"):
-                        sortedArray[3] = prefixArguments[j+1].substring(0, prefixArguments[j+1].length() - 2);
+                        sortedArray[3] = "e/" + argument;
+                        break;
                     case ("a"):
-                        sortedArray[4] = prefixArguments[j+1].substring(0, prefixArguments[j+1].length() - 2);
+                        sortedArray[4] = "a/" + argument;
+                        break;
                     case ("t"):
-                        sortedArray[tagIndex++] = prefixArguments[j+1].substring(0, prefixArguments[j+1].length() - 2);
+                        sortedArray[tagIndex++] = "t/" + argument;
+                        break;
                     default:
                     }
                 }
@@ -124,24 +138,34 @@ public class Processor {
 
             // "COMMAND_WORD + INDEX + PREFIX + DATA" argument pattern
             case EditCommand.COMMAND_WORD:
-                userInput.replace(args[i], "");
+                userInput = userInput.replace(" " + args[i], "");
                 prefixArguments = userInput.split("/", 0);
                 tagIndex = 6;
                 sortedArray[0] = args[i];
                 sortedArray[1] = targetIndex;
-                for (int j = 0; j < prefixArguments.length; j++) {
+                for (int j = 0; j < (prefixArguments.length - 1); j++) {
                     String prefix = prefixArguments[j].substring(prefixArguments[j].length()-1);
+                    if (j == (prefixArguments.length - 2)) {
+                        argument = prefixArguments[j+1];
+                    } else {
+                        argument = prefixArguments[j+1].substring(0, prefixArguments[j+1].length() - 2);
+                    }
                     switch (prefix) {
                     case ("n"):
-                        sortedArray[2] = prefixArguments[j+1].substring(0, prefixArguments[j+1].length() - 2);
+                        sortedArray[2] = "n/" + argument;
+                        break;
                     case ("p"):
-                        sortedArray[3] = prefixArguments[j+1].substring(0, prefixArguments[j+1].length() - 2);
+                        sortedArray[3] = "p/" + argument;
+                        break;
                     case ("e"):
-                        sortedArray[4] = prefixArguments[j+1].substring(0, prefixArguments[j+1].length() - 2);
+                        sortedArray[4] = "e/" + argument;
+                        break;
                     case ("a"):
-                        sortedArray[5] = prefixArguments[j+1].substring(0, prefixArguments[j+1].length() - 2);
+                        sortedArray[5] = "a/" + argument;
+                        break;
                     case ("t"):
-                        sortedArray[tagIndex++] = prefixArguments[j+1].substring(0, prefixArguments[j+1].length() - 2);
+                        sortedArray[tagIndex++] = "t/" + argument;
+                        break;
                     default:
                     }
                 }
